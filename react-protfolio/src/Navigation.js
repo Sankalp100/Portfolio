78ed60eb2e421f2d7af1cd38091ac1b0da6824fd
Sankalp100 <sankalpgour123@gmail.com>
2019-02-04 09:58:09 +0530

    update

---
+++ react-protfolio/src/Navigation.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Navigation extends Component {
+    render() {
+        return(
+            <nav>
+               <h2>React Projects</h2>
+
+               <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contect</li>    
+               </ul> 
+            </nav>
+        );
+    }
+}
+
+export default Navigation
