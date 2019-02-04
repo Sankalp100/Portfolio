6e8bce198d58521300b2483a0aeecd22478451c9
Sankalp100 <sankalpgour123@gmail.com>
2019-02-05 03:18:37 +0530

    update

--- react-protfolio/src/Services.js
+++ react-protfolio/src/Services.js
@@ -10,15 +10,26 @@ class Services extends Component {
                 <div className="row">
                     <div>
                         <span><i class="fas fa-cat"></i></span>
+                        <h4>Responsive</h4>
+                        <p>Look great on any screen size</p>
                     </div>
                     <div>
                         <span><ion-icon name="phone-portrait"></ion-icon></span>
+                        <h4>Responsive</h4>
+                        <p>Look great on any screen size</p>
+
                     </div>
                     <div>
                         <span><ion-icon name="logo-github"></ion-icon></span>
+                        <h4>Open source</h4>
+                        <p>Available on github</p>
+
                     </div>
                     <div>
                         <span><ion-icon name="logo-steam"></ion-icon></span>
+                        <h4>Gaming</h4>
+                        <p>invite me on steam</p>
+
                     </div>
                 </div>
             </div>
