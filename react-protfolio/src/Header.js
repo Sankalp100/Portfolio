4bcb7040b6d1325883c1df5f00cca5c579f0804d
Sankalp100 <sankalpgour123@gmail.com>
2019-02-05 00:00:18 +0530

    update

---
+++ react-protfolio/src/Header.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Header extends Component{
+    const myStyles = {
+        background-image:'./img/ironman.jpg';
+        height: 300px;  
+    }
+    render(){
+        return(
+            <header style={myStyles}>
+
+            </header>
+        );
+    }
+};
+
+export default Header
